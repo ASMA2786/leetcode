8e3142dd9ed14bcf6f80c6473162434176aa1e36
ASMA <shaik.asmaaaa2786@gmail.com>
2026-08-19 23:19:48 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.11%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: str
+        """
+        ans=''
+        for i in range(len(nums)):
+            if nums[i][i]=='0':
+                ans+='1'
+            else:
+                ans+='0'
+        return ans
