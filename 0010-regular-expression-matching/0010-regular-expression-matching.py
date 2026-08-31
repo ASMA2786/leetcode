a263bb70885e16594268e7851b4143d16fdf9b61
ASMA <shaik.asmaaaa2786@gmail.com>
2026-08-31 21:56:42 +0530

    Time: 1335 ms (10.42%), Space: 19.4 MB (49.48%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,40 +1,29 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-
-        memo = {}
-
-        def dfs(i, j):
-
-            # Pattern finished
-            if j == len(p):
-                return i == len(s)
-
-            # Have we already solved this?
-            if (i, j) in memo:
-                return memo[(i, j)]
-
-            # Does current character match?
-            first_match = (
-                i < len(s) and
-                (s[i] == p[j] or p[j] == '.')
-            )
-
-            # If next character is '*'
-            if j + 1 < len(p) and p[j + 1] == '*':
-
-                # Option 1: use 0 occurrences
-                skip = dfs(i, j + 2)
-
-                # Option 2: use 1 or more occurrences
-                use = first_match and dfs(i + 1, j)
-
-                ans = skip or use
-
-            else:
-                # Normal character or '.'
-                ans = first_match and dfs(i + 1, j + 1)
-
-            memo[(i, j)] = ans
-            return ans
-
-        return dfs(0, 0)
+        len_p = len(p)
+        stack = list(p)
+        past = [None]
+        length = len(s)
+        answer = [False]
+        def helper(i,stack):
+            #print(f"{stack} and {s[:i+1]}")
+            if answer[0] or not stack or (i==-1 and not stack[-1] == '*'):
+                if i == -1 and not stack:
+                    answer[0] = True
+                return
+            present = stack.pop()
+            #print(present)
+            if present == s[i]:
+                helper(i-1,stack[:])
+            elif present == '.':
+                helper(i-1,stack[:])
+            elif present == '*':
+                q = stack.pop()
+                for num in range(length+len_p):
+                    stack.extend([q]*num)
+                    helper(i,stack[:])
+                    for _ in range(num): stack.pop()
+        helper(length-1,stack)
+        return answer[0]
+             
+            
